674146a05c684bcf884088f0fe6b1fc0e6f06c0b
sdarkunde <snehaldarkunde95@gmail.com>
2022-07-23 16:31:04 -0400

    commited

---
+++ src/com/packages/main.java
@@ -0,0 +1,22 @@
+package com.packages;
+
+public class main{
+	
+	public static void main(String[] args){
+		int age=30;
+		long viewcount = 3_65_568_234L;
+		float price= 10.4F;
+		char letter ='A';
+		
+		//reference type
+		
+		
+		///string
+		
+		String message= "helo";
+		System.out.println(message.length());
+		
+		
+	}
+}
+
